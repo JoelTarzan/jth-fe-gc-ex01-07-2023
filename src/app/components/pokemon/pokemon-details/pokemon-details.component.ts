02aed8fa4ab27938b137c7844729672e25f77f23
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-18 18:25:05 +0200

    Componente PokemonDetails implementado

--- src/app/components/pokemon/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon/pokemon-details/pokemon-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
   styleUrls: ['./pokemon-details.component.css']
 })
-export class PokemonDetailsComponent {
+export class PokemonDetailsComponent implements OnInit {
+  id: number = 1;
+  pokemon: Pokemon = new Pokemon();
 
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private pokemonService: PokemonService
+    ) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+    });
+
+    this.pokemonService.getById(this.id).subscribe(result => {
+      this.pokemon = result;
+    })
+  }
 }
