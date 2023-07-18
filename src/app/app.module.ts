ef067ab696333da249c507debb509254ab676965
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-18 16:43:10 +0200

    Componentes y Servicios creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PokemonListComponent } from './components/pokemon/pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './components/pokemon/pokemon-card/pokemon-card.component';
+import { PokemonDetailsComponent } from './components/pokemon/pokemon-details/pokemon-details.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PokemonListComponent,
+    PokemonCardComponent,
+    PokemonDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
